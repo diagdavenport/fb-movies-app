3961f6b5cb6adf90c5530b5ff2b8e5df139993a6
Amgad Alzamkan <aalzamkan@gmail.com>
2025-06-23 00:15:58 -0700

    Update frontend to production URL configuration
    - Changed backend_url from localhost:8000 to production server 44.204.168.187
    - Production build ready for deployment
    - Algorithm fixes implemented and tested

--- movies-ui-v2/src/app/global-constants.ts
+++ movies-ui-v2/src/app/global-constants.ts
@@ -1,5 +1,5 @@
 export class GlobalConstants {
-    // Original production URL: "http://44.204.168.187/"
-    public static backend_url: string = "http://localhost:8000/";
+    // Development URL: "http://localhost:8000/"
+    public static backend_url: string = "http://44.204.168.187/";
      
 }
