440c6842df1c3a7837bdc8cf63e697db4f09a1e0
Amgad Alzamkan <aalzamkan@gmail.com>
2025-06-26 21:14:13 -0700

    Update global-constants.ts

--- movies-ui-v2/src/app/global-constants.ts
+++ movies-ui-v2/src/app/global-constants.ts
@@ -1,5 +1,5 @@
 export class GlobalConstants {
     // Development URL: "http://localhost:8000/"
-    public static backend_url: string = "http://44.204.168.187/";
+    public static backend_url: string = "http://44.204.168.187:8000/";
      
 }
